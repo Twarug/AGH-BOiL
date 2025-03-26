b5f65723831407aaedc316b1cb884fc22d629d35
Twarug <patryk@twardosz.dev>
2025-03-26 21:34:15 +0100

    Add input_data -> Graph conversion

--- src/lib/Graph.ts
+++ src/lib/Graph.ts
@@ -21,3 +21,26 @@ export type CPM_Node = {
 export type CPM_Graph = {
   nodes: CPM_Node[]
 }
+
+const convertGraphData = (data: CPM_Data[]): CPM_Graph => {
+  const nodes: CPM_Node[] = data.map((node, i) => ({
+    id: i,
+    name: node.name,
+    duration: node.duration,
+    es: 0,
+    ef: 0,
+    ls: 0,
+    lf: 0,
+    critical: false,
+    predecessors: node.dependencies,
+    successors: []
+  }));
+
+  nodes.forEach(node => {
+    node.predecessors.forEach(p => nodes[p].successors.push(node.id));
+  });
+
+  return {
+    nodes: nodes,
+  };
+}
